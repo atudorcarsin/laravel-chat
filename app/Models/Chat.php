7e241951d0d7c229734bf2b7136143e7ac6b0dc5
Tudor Carsin <110934835+blue892@users.noreply.github.com>
2024-09-25 21:47:00 -0400

    Create relationship between user and chat

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Chat extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function userOne(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_one_id');
+    }
+
+    public function userTwo(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_two_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +46,20 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function chats()
+    {
+        return Chat::where('user_one_id', $this->id)->orWhere('user_two_id', $this->id)->get();
+    }
+
+    public function chatsAsUserOne(): HasMany
+    {
+        return $this->hasMany(Chat::class, 'user_one_id');
+    }
+
+    public function chatsAsUserTwo(): HasMany
+    {
+        return $this->hasMany(Chat::class, 'user_two_id');
     }
 }
